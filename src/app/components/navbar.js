82501bbf606f4c5fbf47a0c07af366f977df05e8
Martin Otero <martin_otero@hotmail.com>
2024-03-15 12:10:35 -0300

    navbar

---
+++ src/app/components/navbar.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import logo from "../../../images/logo.svg";
+
+export default function Navbar() {
+  return (
+    <div className=" bg-red-600 w-full h-16">
+      <div className="m-6">
+        <Image src={logo} alt="logo" width={84} height={27} className=" " />
+      </div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,29 @@
 import Image from "next/image";
+import Navbar from "./components/navbar";
+import heroD from "../../images/image-hero-desktop.png";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
-      <div className=" bg-red-600 w-full h-16">Navbar</div>
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"></div>
+      <Navbar />
+      <div className="w-full items-center justify-between font-mono text-sm lg:flex">
+        <div className="grid-cols-1 ">
+          <div className="block w-[780px]">
+            <h1 className="text-[40px] ">Make remote work</h1>
+            <p className="text-[18px] ">
+              Get your team in sync, no matter your location.
+            </p>
+            <button>Learn More</button>
+          </div>
+          <Image
+            src={heroD}
+            alt="heroD"
+            width={780}
+            height={980}
+            className=" "
+          />
+        </div>
+      </div>
 
       {/*Footer*/}
       <div className="mt-auto mb-4 text-[13px] sm:text-[16px] w-[375px] sm:w-[440px]">
